900a1ccda8e5a785ff2b0f533cb9074dac89874e
Diego <digomfd@gmail.com>
2024-08-16 01:46:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById('button')
+let body = document.querySelector('body')
+
+
+button.addEventListener('click', ()=>{
+    button.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
+    
