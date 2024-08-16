4c3ee3090b948e15327a2cca7de65200c660734f
Diego <digomfd@gmail.com>
2024-08-16 02:02:45 -0300

    códigos formatados

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ let button = document.getElementById('button')
 let body = document.querySelector('body')
 
 
-button.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
     button.classList.toggle('dark')
     body.classList.toggle('dark')
 })
-    
